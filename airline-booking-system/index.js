cbcda8caec789b097d6093addb05d0a0be141e44
mariefilabawag <mariefilabawag@gmail.com>
2025-03-21 22:47:12 +0800

    Add files via upload

---
+++ airline-booking-system/index.js
@@ -0,0 +1,12 @@
+/* Input the correct syntax */
+$(document).ready(function() {
+    // Set the real-time date
+    const today = new Date();
+    const formattedDate = today.toLocaleDateString('en-GB'); // Format: DD/MM/YYYY
+    $(".bi-clock").parent().text(` ${formattedDate}`);
+    
+    // Confirm button functionality
+    $('.btn-confirm').click(function() {
+        alert('Payment confirmed!');
+    });
+}); 
